962b3a36082e024ca0e7d65d8fad395bd613eb5f
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-03-18 14:04:01 +0530

    dfgfdgfdg

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Assam News Portal</title>
+    <title>India Tomorrow : Focussing Education</title>
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="keywords" content="">
