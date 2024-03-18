3f225d521246b7961005226029569966fe253de0
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-03-18 14:27:52 +0530

    gfhgfhgfh

--- resources/views/layouts/front/image-video-show.blade.php
+++ resources/views/layouts/front/image-video-show.blade.php
@@ -13,13 +13,14 @@
 @endphp
 @if ($news->photo_or_video == 'photo')
     <a href="{{ route($cat_array[0], [$news->news_slug]) }}" class="thumb">
-        <img src="{{ asset($news->image) }}" alt="{{ $news->news_slug }}" width="600" height="300" onload="pushImage({{ asset($news->image) }})">
+        <img src="{{ asset($news->image) }}" alt="{{ $news->news_slug }}" width="600" height="300">
     </a>
 @else
     <a href="#" class="thumb" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="myfunction('{{ $news->youtube_url }}', '{{ $news->id }}')">
         <img src="https://img.youtube.com/vi/{{ $substring }}/0.jpg" alt="$cat_array[0]" width="600" height="300">
         <div class="play-button"></div>
     </a>
+
 @endif
 
 
