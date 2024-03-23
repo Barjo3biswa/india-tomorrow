f40f8d9b58c6487b76be9be513efde833227afd7
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-03-23 12:30:33 +0530

    dgdfgfdgdfg

--- resources/views/view-news.blade.php
+++ resources/views/view-news.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.front.app')
+@section('shareable')
+@if ($news->photo_or_video == 'photo')
+    <a href="{{ route($cat_array[0], [$news->news_slug]) }}" class="thumb">
+        <img src="{{ asset($news->image) }}" alt="{{ $news->news_slug }}" width="600" height="300">
+    </a>
+@else
+    <a href="#" class="thumb" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="myfunction('{{ $news->youtube_url }}', '{{ $news->id }}')">
+        <img src="https://img.youtube.com/vi/{{ $substring }}/0.jpg" alt="$cat_array[0]" width="600" height="300">
+        <div class="play-button"></div>
+    </a>
+@endif
+@endsection
 @section('content')
 <div class="container">
     <div class="ad--space pd--20-0-20" style="padding-top: 2rem;">
