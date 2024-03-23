e8fe5e78d42bbec0da87d3c1200b779243c5a829
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-03-23 12:46:00 +0530

    gfhgfhgfh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('shareable')
     <meta name="description" content="India-Tomorrow-Admin">
-    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
+    <meta name="author" content="web.com.(India)">
     <link rel="shortcut icon" href="favicon.ico">
 
     <!-- FontAwesome JS-->
--- resources/views/view-news.blade.php
+++ resources/views/view-news.blade.php
@@ -1,21 +1,15 @@
 @extends('layouts.front.app')
 @section('shareable')
-@if ($news->photo_or_video == 'photo')
-    {{-- <a href="{{ route($cat_array[0], [$news->news_slug]) }}" class="thumb">
-        <img src="{{ asset($news->image) }}" alt="{{ $news->news_slug }}" width="600" height="300">
-    </a> --}}
-    <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
-    <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-    <meta property="og:image" content="{{ asset($news->image) }}" />
-@else
-    {{-- <a href="#" class="thumb" data-toggle="modal" data-target=".bd-example-modal-lg" onclick="myfunction('{{ $news->youtube_url }}', '{{ $news->id }}')">
-        <img src="https://img.youtube.com/vi/{{ $substring }}/0.jpg" alt="$cat_array[0]" width="600" height="300">
-        <div class="play-button"></div>
-    </a> --}}
-    <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
-    <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-    <meta property="og:image" content="https://img.youtube.com/vi/{{ $substring }}/0.jpg" />
-@endif
+    <meta property="og:title" content="{{$news->news_title}}">
+    <meta property="og:description" content="{{$news->news_title}}">
+    @if ($news->photo_or_video == 'photo')
+        <meta property="og:image" content="{{ asset($news->image) }}">
+    @else
+        <meta property="og:image" content="https://img.youtube.com/vi/{{ $substring }}/0.jpg">
+    @endif
+    {{-- <meta property="og:url" content="Your Page URL Here"> --}}
+    <meta property="og:type" content="website">
+
 @endsection
 @section('content')
 <div class="container">
